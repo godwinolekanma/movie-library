7082b538915b18e9a88ddceb79be6cc3bc3cc673
godwinolekanma <godwinolekanma12@gmail.com>
2024-05-04 17:44:10 -0500

    Add procfile and postgresql uri

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Movie.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///Movie.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
